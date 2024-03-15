0e1b0fc34072fd79d746b920f2b41de9bb39a5c3
Sunphetcharaj <Sunphetcharaj123@gmail.com>
2024-03-14 19:24:11 -0700

    w

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const app = express()
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
+app.use(express.static('public'))
+
 app.use('/places', require('./controllers/places.js'))
 
 app.get('/', (req, res) => {
