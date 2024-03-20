2ca654c1d6c685ee55d887af1386f627e3204782
Sunphetcharaj <Sunphetcharaj123@gmail.com>
2024-03-19 20:09:45 -0700

    w

--- controllers/places.js
+++ controllers/places.js
@@ -2,35 +2,27 @@ const router = require('express').Router()
 const places = require('../models/places.js')
 
 router.get('/new', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/restaurant.webp'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/Dining-Room'
-      }]
-    res.render('places/new', {places})
+    res.render('places/new')
+})
+
+router.get('/', (req, res) => {
+    res.render('places/index', { places })
 })
 
 router.post('/', (req, res) => {
-  if (!req.body.pic) {
-    // Default image if one is not provided
-    req.body.pic = 'http://placekitten.com/400/400'
-  }
-  if (!req.body.city) {
-    req.body.city = 'Anytown'
-  }
-  if (!req.body.state) {
-    req.body.state = 'USA'
-  }
-  places.push(req.body)
-  res.redirect('/places')
+    console.log(req.body)
+    if (!req.body.pic) {
+      // Default image if one is not provided
+        req.body.pic = 'https://placehold.co/400'
+    }
+    if (!req.body.city) {
+        req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+        req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
 })
 
 
