395d2fb6cefd75b4ca3765166377cd4ad08365a7
Sunphetcharaj <Sunphetcharaj123@gmail.com>
2024-03-26 18:34:13 -0700

    Up to date with part 9

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,13 +1,19 @@
+const React = require('react')
+const Def = require('../default')
+
 function index (data) {
     let placesFormatted = data.places.map((place) => {
       return (
         <div className="col-sm-6">
           <h2>{place.name}</h2>
-          <p className="text-center">
-            {place-cuisines}
+          <a href={'/places/${place.id}'}>
+            {place.name}
+          </a>
+          <p className='text-center'>
+            {place.cuisines}
           </p>
           <img src={place.pic} alt={place.name}/>
-          <p className="text-center">
+          <p className='text-center'>
             Located in {place.city}, {place.state}
           </p>
         </div>
@@ -16,11 +22,18 @@ function index (data) {
     return (
       <Def>
           <main>
-              <h1>PLACES INDEX PAGE</h1>
+              <h1>Places to Rant About</h1>
               <div className="row">
+              <div className="form-group">
+  <label for="founded">Founded Year</label>
+  <input className="form-control" id="founded" name="founded" />
+</div>
               {placesFormatted}
               </div>
           </main>
       </Def>
   )
   }
+
+  module.exports = index
+  
