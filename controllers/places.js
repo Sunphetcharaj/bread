f8ed88f260f97ed6cf39c680cd4c625d114f6f25
Sunphetcharaj <Sunphetcharaj123@gmail.com>
2024-04-04 18:47:09 -0700

    part 11 finished

--- controllers/places.js
+++ controllers/places.js
@@ -42,7 +42,7 @@ router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
   .populate('comments')
   .then(place => {
-      console.log(place.comments)
+    console.log(place.comments)
       res.render('places/show', { place })
   })
   .catch(err => {
@@ -53,45 +53,68 @@ router.get('/:id', (req, res) => {
 
 
 router.put('/:id', (req, res) => {
-  res.send('PUT /places/:id stub')
+  db.Place.findByIdAndUpdate(req.params.id, req.body)
+  .then(() => {
+      res.redirect(`/places/${req.params.id}`)
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
 })
+
 
 router.delete('/:id', (req, res) => {
   res.send('DELETE /places/:id stub')
 })
 
 router.get('/:id/edit', (req, res) => {
-  res.send('GET edit form stub')
+  db.Place.findById(req.params.id)
+  .then(place => {
+      res.render('places/edit', { place })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
 })
+
 
 router.post('/:id/rant', (req, res) => {
   res.send('GET /places/:id/rant stub')
 })
 
-router.delete('/:id/rant/:rantId', (req, res) => {
-    res.send('GET /places/:id/rant/:rantId stub')
+router.delete('/:id', (req, res) => {
+  db.Place.findByIdAndDelete(req.params.id)
+  .then(place => {
+      res.redirect('/places')
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
 })
 
-function show (data) {
-  let comments = (
-    <h3 className="inactive">
-      No comments yet!
-    </h3>
-  )
-  return (
-      <Def>
-        <main>
-          <div className="row">
-            ...
-          </div>
-          <hr />
-          <h2>Comments</h2>
-          {comments}
-        </main>
-      </Def>
-  )
-}
-
-module.exports = show
+router.post('/:id/comment', (req,res) => {
+  console.log(req.body)
+  req.body.rant = req.body.rant ? true : false
+  db.Place.findById(req.params.id)
+  .then(place => {
+    db.Comment.create(req.body)
+    .then(comment => {
+      place.comments.push(comment.id)
+      place.save()
+      .then (() => {
+        res.redirect('/places/${req.params.id')
+      })
+    })
+    .catch(err => {
+      res.render('error404')
+    })
+  })
+  .catch(err=> {
+    res.render('error404')
+  })
+  res.send('GET /places/:id/comment stub')
+})
 
 module.exports = router
